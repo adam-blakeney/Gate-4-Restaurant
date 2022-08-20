65317bed4017b4559e128f03252316c0789011d8
adam-blakeney <adam.blakeney@outlook.com>
2022-08-20 15:04:18 +0000

    get-reserve-list

--- bluegrotto/urls.py
+++ bluegrotto/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from reserve.views import get_reserve_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', get_reserve_list, name='get_reserve_list')
 ]
--- reserve/views.py
+++ reserve/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def get_reserve_list(request):
+    return render(request, 'reserve/reserve_list.hmtl')
+
+
