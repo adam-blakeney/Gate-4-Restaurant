c538443e4c8c11c84d655d958d0fc80aaadc462b
adam-blakeney <adam.blakeney@outlook.com>
2022-08-20 16:37:55 +0000

    errors

--- bluegrotto/asgi.py
+++ bluegrotto/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for bluegrotto project.
+ASGI config for gate4 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- bluegrotto/urls.py
+++ bluegrotto/urls.py
@@ -17,10 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
-    path('', include('home.urls')),
+    # path('accounts/', include('allauth.urls')),
+    # path('', include('home.urls')),
 
  ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
