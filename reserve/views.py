1e64d2b837de9564e33413f7288c4140561a1780
adam-blakeney <adam.blakeney@outlook.com>
2022-08-20 15:51:47 +0000

    added html to reserve list and base.html structure

--- reserve/views.py
+++ reserve/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-
+from .models import Item
 # Create your views here.
 
+
 def get_reserve_list(request):
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
     return render(request, 'reserve/reserve_list.hmtl')
-
-
